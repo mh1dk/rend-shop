870c18530943a262c053810fbfaad2707d3c4b7e
Mahdi Karimi <mhdksp566@gmail.com>
2024-12-06 15:45:15 +0330

    responsive updated

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,27 +1,27 @@
-  function animateValue(obj, start, end, duration) {
+function animateValue(obj, start, end, duration) {
     let startTimestamp = null;
     const step = (timestamp) => {
-      if (!startTimestamp) startTimestamp = timestamp;
-      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      obj.innerHTML = Math.floor(progress * (end - start) + start);
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      }
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
     };
     window.requestAnimationFrame(step);
-  }
+}
 
-  const elements = document.querySelectorAll(".value");
-  const startAnimation = () => {
+const elements = document.querySelectorAll(".value");
+const startAnimation = () => {
     elements.forEach((obj) => {
-      const rect = obj.getBoundingClientRect();
-      const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
+        const rect = obj.getBoundingClientRect();
+        const isVisible = rect.top <= window.innerHeight && rect.bottom >= 0;
 
-      if (isVisible && !obj.classList.contains("animated")) {
-        animateValue(obj,0, 100, 3500);
-        obj.classList.add("animated");
-      }
+        if (isVisible && !obj.classList.contains("animated")) {
+            animateValue(obj, 0, 100, 3500);
+            obj.classList.add("animated");
+        }
     });
-  };
+};
 
-  window.addEventListener("scroll", startAnimation);
+window.addEventListener("scroll", startAnimation);
